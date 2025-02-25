d7b91ac836560135a0da8b8b6b824fb069977722
Md Oliul Islam Rian <oliulam03@gmail.com>
2025-02-26 01:25:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("SECRET_K")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
